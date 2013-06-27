32512b3efdc43dbdc5d42350a24c914b33e12b4b
Ian Brandt <ian@ianbrandt.com>
2013-06-27 16:16:31 -0700

    Reverting back to deferred closing, but inline with StaxEventItemWriter.close() to avoid a chicken and egg problem with resource close order.

--- src/main/java/com/ianbrandt/spring/batch/test/SpringOxmWorkaroundStaxEventItemWriter.java
+++ src/main/java/com/ianbrandt/spring/batch/test/SpringOxmWorkaroundStaxEventItemWriter.java
@@ -6,28 +6,12 @@ import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.xml.StaxEventItemWriter;
 import org.springframework.batch.item.xml.stax.DeferredCloseEventWriterWrapper;
-import org.springframework.core.io.Resource;
 
 public class SpringOxmWorkaroundStaxEventItemWriter<T> extends StaxEventItemWriter<T> {
 
-	private static final Log log = LogFactory.getLog(SpringOxmWorkaroundStaxEventItemWriter.class);
-
 	private DeferredCloseEventWriterWrapper deferredCloseEventWriterWrapper;
-
-	private Resource resource;
-
-
-	@Override
-	public void setResource(final Resource resource) {
-
-		this.resource = resource;
-
-		super.setResource(resource);
-	}
 
 	@Override
 	protected XMLEventWriter createXmlEventWriter(final XMLOutputFactory outputFactory, final Writer writer)
@@ -43,17 +27,10 @@ public class SpringOxmWorkaroundStaxEventItemWriter<T> extends StaxEventItemWrit
 	@Override
 	public void close() {
 
-		super.close();
-
 		if (deferredCloseEventWriterWrapper != null) {
-
-			try {
-
-				deferredCloseEventWriterWrapper.deferredClose();
-
-			} catch (XMLStreamException e) {
-				log.error("Unable to close file resource: [" + resource + "] " + e);
-			}
+			deferredCloseEventWriterWrapper.setClose(true);
 		}
+
+		super.close();
 	}
 }
--- src/main/java/org/springframework/batch/item/xml/stax/DeferredCloseEventWriterWrapper.java
+++ src/main/java/org/springframework/batch/item/xml/stax/DeferredCloseEventWriterWrapper.java
@@ -5,18 +5,22 @@ import javax.xml.stream.XMLStreamException;
 
 public class DeferredCloseEventWriterWrapper extends AbstractEventWriterWrapper {
 
+	private boolean close = false;
+
 	public DeferredCloseEventWriterWrapper(final XMLEventWriter wrappedEventWriter) {
 		super(wrappedEventWriter);
+	}
+
+	public void setClose(boolean close) {
+
+		this.close  = close;
 	}
 
 	@Override
 	public void close() throws XMLStreamException {
 
-		// No-op.
-	}
-
-	public void deferredClose() throws XMLStreamException {
-
-		super.close();
+		if (close) {
+			super.close();
+		}
 	}
 }
